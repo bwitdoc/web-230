130b46d4ab02537e541f7fa8d7386774ee8bcf36
bwitdoc <bwitdoc@gmail.com>
2019-05-29 19:00:40 -0700

    Adding assignment 1.4

---
+++ assignment-1.4/dockter-assignment-1.4.js
@@ -0,0 +1,7 @@
+var firstName = "Brittany"
+var lastName = "Dockter"
+var age = "22"
+var street = "1003 Long Dr"
+var city = "Santee"
+var zipCode = "92071"
+console.log(firstName + "\n" + lastName + "\n" + age + "\n" + street + "\n" + zipCode)
