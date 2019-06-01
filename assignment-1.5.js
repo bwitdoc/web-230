1496ec6db6fefb608aac425179120dfc4911e9d4
Brittany Dockter <bwitdoc@gmail.com>
2019-06-01 10:05:07 -0700

    Assignment 1.5
    Employee file program

---
+++ assignment-1.5.js
@@ -0,0 +1,47 @@
+/* 
+=====================
+Title: Assignment 1.5
+Author: Brittany Dockter
+Date: June 1, 2019
+Modified by:
+Description: A short program of employee information.
+=====================
+*/
+
+/* Expected output:
+* John
+* Smith
+* 73 Dover Pl
+* 24.70
+* Fri April 19, 2019 00:00:0
+0 GMT-0700 (Pacific Day 
+* light time)
+*/
+
+// Begin Program
+
+var firstName = "John";
+var lastName = "Smith";
+var address = "73 Dover Pl.";
+var payRate = "24.70";
+var hireDate = new Date (2013, 03, 19)
+
+console.log(firstName + "\n" + lastName + "\n" + address + "\n" + payRate + "\n" + hireDate)
+
+var firstName = "Ryan";
+var lastName = "Ortiz";
+var address = "151 1st Ave";
+var payRate = "19.50";
+var hireDate = new Date (2017, 08, 21);
+
+console.log(firstName + "\n" + lastName + "\n" + address + "\n" + payRate + "\n" + hireDate)
+
+var firstName = "Elizabeth";
+var lastName = "Leaman";
+var address = "26 Pepper Dr";
+var payRate = "22.35";
+var hireDate = new Date (2012, 12, 04);
+
+console.log(firstName + "\n" + lastName + "\n" + address + "\n" + payRate + "\n" + hireDate)
+
+// End Program
