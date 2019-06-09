314690c2f65c4f51c0c3de844f6a19d34c9ed25a
Brittany Dockter <bwitdoc@gmail.com>
2019-06-09 13:31:50 -0700

    Adding header files

---
+++ Dockter-header-test.js
@@ -0,0 +1,17 @@
+/*
+============================================
+; Title:  header-test.js
+; Author: Brittany Dockter 
+; Date:   09 June 2019
+; Modified by: Brittany Dockter
+; Description: Displays a formatted header
+;===========================================
+*/
+
+// Begin Program
+
+const header = require('./header.js');
+
+console.log(header.display("Brittany", "Dockter", "Assignment-2.2"));
+
+// End Program
