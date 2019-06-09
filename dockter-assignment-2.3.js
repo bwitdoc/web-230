db07d41a52cb598e558bd6a1910d233cbb78e55c
Brittany Dockter <bwitdoc@gmail.com>
2019-06-09 13:53:17 -0700

    Adding commit for assignment 2-3

---
+++ dockter-assignment-2.3.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title:  Assignment 2.3
+; Author: Brittany Dockter 
+; Date:   09 June 2019
+; Modified By: Brittany Dockter
+; Description: Creating a basic function
+;===========================================
+*/
+
+/* Expected output:
+
+; Hello Brittany Dockter!
+
+*/
+
+// Label Properties and Variables
+var greeting ="Hello ";
+myName.Brittany = "Brittany ";
+var lastName = "Dockter!";
+
+// Begin function
+function myName() {
+    return myName.Brittany;
+}
+
+// Begin Output
+console.log(greeting + myName() + lastName)
+
+// End program
