adf6a1c951dc53e8a2292532c891784ad1af43ba
Brittany Dockter <bwitdoc@gmail.com>
2019-06-13 19:03:10 -0700

    Adding broken discussion 3.1

---
+++ dockter-discussion-3-broken.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  Discussion 3.1
+; Author: Brittany Dockter 
+; Date:   13 June 2019
+; Modified by: Brittany Dockter
+; Description: A simple if else statement
+;===========================================
+*/
+
+// Begin Header
+var header = require('C:/Users/Brittany/Documents/WEB231/bu-webdev/web-231/week-2/Dockter-header-test');
+// End Header
+
+// Begin Program
+var brittany = "dockter";
+var dockter = "lastName";
+
+
+if (brittany == dockter) {
+    if (dockter == lastname) {
+        console.log("brittany equals lastName");
+    }
+}
+else {
+    console.log("brittany doesn't equal dockter");
+}
