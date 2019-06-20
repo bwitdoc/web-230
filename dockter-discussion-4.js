5d813b64ba941de8d7dbb0cb71591f56e23639a5
Brittany Dockter <bwitdoc@gmail.com>
2019-06-20 16:52:34 -0700

    adding commit for discussion 4

---
+++ dockter-discussion-4.js
@@ -0,0 +1,33 @@
+/*
+============================================
+; Title:  Discussion 4.1
+; Author: Brittany Dockter 
+; Date:   20 June 2019
+; Modified by: Brittany Dockter
+; Description: A simple program using Arrays that contain two errors.
+;===========================================
+*/
+
+// Begin Header
+var header = require('C:/Users/Brittany/Documents/WEB231/bu-webdev/web-231/week-2/Dockter-header-test');
+// End Header
+
+/* Expected Output
+
+Brittany Dockter
+Discussion 4.1
+Date: 06/20/2019
+My favorite food is chicken
+
+*/
+
+// Begin Program
+
+var myfavorite = "My favorite food is "
+var meat = ["steak", "chicken", "pork"];
+meat["2"]
+
+
+// Output
+
+console.log(myFavorite, meat[2]);
