d9aa1a247343636b58a8e2c38c9b3c2df02c377a
Brittany Dockter <bwitdoc@gmail.com>
2019-06-25 19:16:04 -0700

    Adding commit assignment 5.1

---
+++ dockter-assignment-5.1.js
@@ -0,0 +1,27 @@
+/*
+============================================
+; Title:  Assignment 5.1
+; Author: Brittany Dockter 
+; Date:   23 June 2019
+; Modified by: Brittany Dockter
+; Description: Creating a function using maps.
+;===========================================
+*/
+
+// Begin Header
+var header = require('C:/Users/Brittany/Documents/WEB231/bu-webdev/web-231/week-2/Dockter-header-test');
+// End Header
+
+/* --- Expected Output ---
+Array(5) [7, 9, 11, 13, 15]
+*/
+
+// Begin Program
+
+var array1 = "[2, 4, 6, 8, 10]";
+var map1 = array1.map(x = x + 5);
+
+// Output
+console.log(map)
+
+// End Program
