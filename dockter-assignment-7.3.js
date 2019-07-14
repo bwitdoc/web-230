592b025f1d5756b8992d7a72825f02e9c5a4ed13
Brittany Dockter <bwitdoc@gmail.com>
2019-07-14 10:57:37 -0700

    adding commit for assignment 7.3

---
+++ dockter-assignment-7.3.js
@@ -0,0 +1,55 @@
+/*
+============================================
+; Title:  Exercise 7.3
+; Author: Brittany Dockter 
+; Date:   14 July 2019
+; Modified by: Brittany Dockter
+; Description: Embedded JavaScript in HTML 
+;===========================================
+*/
+
+// Begin Header
+var header = require('C:/Users/Brittany/Documents/WEB231/bu-webdev/web-231/week-2/Dockter-header-test');
+// End Header
+
+// Start Program
+
+<script>
+    document.write("<h1> SHOPPING LIST v.1</h1>")
+    document.write("<ol>
+        1. Oranges
+        2. Apples
+        3. Pears
+        4. Limes
+        5. Lemons</ol>")
+</script>
+
+<h1> SHOPPING LIST v.2</h1>
+<table>
+<tr>
+    <td>Id</td>
+    <td>Item</td>
+ </tr>
+ <tr>
+     <td>1.</td>
+     <td>Oranges</td>
+ </tr>
+<tr>
+    <td>2.</td>
+    <td>Apples</td>
+</tr>
+<tr>
+    <td>3.</td>
+    <td>Pears</td>
+</tr>
+<tr>
+    <td>4.</td>
+    <td>Limes</td>
+</tr>
+<tr>
+    <td>5.</td>
+    <td>Lemons</td>
+</tr>
+</table>
+
+// End Program
